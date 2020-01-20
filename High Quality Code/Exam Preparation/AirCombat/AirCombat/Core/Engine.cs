5937d406a355ce9f9f471d2d322341a52cc1366f
krgizdov <lumpkinzg@gmail.com>
2020-01-20 14:35:54 +0200

    Modified output

--- High Quality Code/Exam Preparation/AirCombat/AirCombat/Core/Engine.cs
+++ High Quality Code/Exam Preparation/AirCombat/AirCombat/Core/Engine.cs
@@ -2,6 +2,7 @@
 {
     using System;
     using System.Linq;
+    using System.Text;
     using AirCombat.Utils;
     using Contracts;
     using IO.Contracts;
@@ -28,21 +29,20 @@
         public void Run()
         {
             this.isRunning = true;
-
+            StringBuilder builder = new StringBuilder();
             while (this.isRunning)
             {
                 var line = this.reader.ReadLine();
                 var cmds = line.Split(" ", StringSplitOptions.RemoveEmptyEntries).ToList();
                 var commandName = cmds[0];
                 var result = this.commandInterpreter.ProcessInput(cmds);
-                this.writer.WriteLine(result);
-
+                //this.writer.WriteLine(result);
+                builder.AppendLine(result);
                 if (commandName == GlobalConstants.TerminateCommand)
                 {
                     this.isRunning = false;
+                    this.writer.WriteLine(builder.ToString());
                 }
-
-
             }
         }
     }
