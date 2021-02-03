7debb1c383b63d144136e2a31dc35bdd1e6e9309
krgizdov <lumpkinzg@gmail.com>
2021-02-03 15:51:54 +0200

    Finished with Graphs, Strongly Connected Components And Max Flow lab tasks.

---
+++ Algorithms with C#/Algorithms Advanced/GraphsStronglyConnectedComponentsMaxFlow.Lab/ArticulationPoints/Program.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace ArticulationPoints
+{
+    class Program
+    {
+        private static List<int>[] _graph;
+        private static int[] _depth;
+        private static int[] _lowPoint;
+        private static int[] _parent;
+        private static bool[] _visited;
+        private static List<int> _articulationPoints;
+
+        static void Main()
+        {
+            var nodes = int.Parse(Console.ReadLine());
+            var lines = int.Parse(Console.ReadLine());
+
+            _graph = ReadGraph(nodes, lines);
+            _depth = new int[nodes];
+            _lowPoint = new int[nodes];
+            _parent = new int[nodes];
+            _visited = new bool[nodes];
+            _articulationPoints = new List<int>();
+
+            Array.Fill(_parent, -1);
+
+            for (int node = 0; node < _graph.Length; node++)
+            {
+                if (!_visited[node])
+                {
+                    FindArticulationPoint(node, 1);
+                }
+            }
+
+            Console.WriteLine($"Articulation points: {string.Join(", ", _articulationPoints)}");
+        }
+
+        private static void FindArticulationPoint(int node, int depth)
+        {
+            _visited[node] = true;
+            _lowPoint[node] = depth;
+            _depth[node] = depth;
+
+            var childCount = 0;
+            var isArticulationPoint = false;
+
+            foreach (var child in _graph[node])
+            {
+                if (!_visited[child])
+                {
+                    _parent[child] = node;
+                    FindArticulationPoint(child, depth + 1);
+                    childCount++;
+
+                    if (_lowPoint[child] >= depth)
+                    {
+                        isArticulationPoint = true;
+                    }
+
+                    _lowPoint[node] = Math.Min(_lowPoint[node], _lowPoint[child]);
+                }
+                else if (_parent[node] != child)
+                {
+                    _lowPoint[node] = Math.Min(_lowPoint[node], _depth[child]);
+                }
+            }
+
+            if (_parent[node] == -1 && childCount > 1 || 
+                _parent[node] != -1 && isArticulationPoint)
+            {
+                _articulationPoints.Add(node);
+            }
+        }
+
+        private static List<int>[] ReadGraph(int nodes, int lines)
+        {
+            var graph = new List<int>[nodes];
+
+            for (int node = 0; node < nodes; node++)
+            {
+                graph[node] = new List<int>();
+            }
+
+            for (int i = 0; i < lines; i++)
+            {
+                var line = Console.ReadLine()
+                    .Split(", ")
+                    .Select(int.Parse)
+                    .ToArray();
+
+                var firstNode = line[0];
+
+                for (int child = 1; child < line.Length; child++)
+                {
+                    graph[firstNode].Add(line[child]);
+                }
+            }
+
+            return graph;
+        }
+    }
+}
---
+++ Algorithms with C#/Algorithms Advanced/GraphsStronglyConnectedComponentsMaxFlow.Lab/MaxFlow/Program.cs
@@ -0,0 +1,132 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace MaxFlow
+{
+    class Program
+    {
+        private static int[,] _graph;
+        private static int[] _parents;
+
+        static void Main()
+        {
+            var nodes = int.Parse(Console.ReadLine());
+
+            _graph = ReadGraph(nodes);
+
+            var source = int.Parse(Console.ReadLine());
+            var target = int.Parse(Console.ReadLine());
+
+            _parents = new int[nodes];
+            Array.Fill(_parents, -1);
+
+            var maxFlow = FindMaxFlow(source, target);
+
+            Console.WriteLine($"Max flow = {maxFlow}");
+        }
+
+        private static int FindMaxFlow(int source, int target)
+        {
+            var maxFlow = 0;
+
+            while (BFS(source, target))
+            {
+                var currentFlow = GetCurrentFlow(source, target);
+
+                maxFlow += currentFlow;
+
+                ApplyCurrentFlow(source, target, currentFlow);
+            }
+
+            return maxFlow;
+        }
+
+        private static void ApplyCurrentFlow(int source, int target, int currentFlow)
+        {
+            var node = target;
+
+            while (node != source)
+            {
+                var parent = _parents[node];
+
+                _graph[parent, node] -= currentFlow;
+
+                node = parent;
+            }
+        }
+
+        private static int GetCurrentFlow(int source, int target)
+        {
+            var node = target;
+            var minFlow = int.MaxValue;
+
+            while (node != source)
+            {
+                var parent = _parents[node];
+
+                var flow = _graph[parent, node];
+
+                if (flow < minFlow)
+                {
+                    minFlow = flow;
+                }
+
+                node = parent;
+            }
+
+            return minFlow;
+        }
+
+        private static bool BFS(int source, int target)
+        {
+            var queue = new Queue<int>();
+            var visited = new bool[_graph.GetLength(0)];
+
+            queue.Enqueue(source);
+            visited[source] = true;
+
+            while (queue.Count > 0)
+            {
+                var node = queue.Dequeue();
+
+                if (node == target)
+                {
+                    return true;
+                }
+
+                for (int child = 0; child < _graph.GetLength(1); child++)
+                {
+                    if (!visited[child] && _graph[node, child] > 0)
+                    {
+                        visited[child] = true;
+                        queue.Enqueue(child);
+                        _parents[child] = node;
+                    }
+                }
+            }
+
+            return false;
+        }
+
+        private static int[,] ReadGraph(int nodes)
+        {
+            var graph = new int[nodes, nodes];
+
+            for (int node = 0; node < nodes; node++)
+            {
+                var capacities = Console.ReadLine()
+                    .Split(", ")
+                    .Select(int.Parse)
+                    .ToArray();
+
+                for (int child = 0; child < capacities.Length; child++)
+                {
+                    graph[node, child] = capacities[child];
+                }
+            }
+
+            return graph;
+        }
+    }
+}
